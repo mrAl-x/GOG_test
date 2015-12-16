487f4712a97552d836536e48684fe157fa4c9722
Alexandre Ferreira <hello@alexontheweb.me>
2015-12-16 15:27:57 +0000

    - Changing sold games value locks/unlocks milestones

--- frontend_source/js/modules/GamesSold.js
+++ frontend_source/js/modules/GamesSold.js
@@ -2,7 +2,7 @@ var App = App || {};		// If App is not defined make it an empty object
 
 App.GamesSold = (function() {
 
-	var totalSells = '22576';
+	var totalSells = '120000';
 
 	var gamesSold = function() {
 
@@ -22,12 +22,52 @@ App.GamesSold = (function() {
 			}
 		}
 
-	}
+	};
+
+	var unlock = function(num) {
+		document.querySelectorAll('.sold__item--' + num)[0].classList.add('sold__check--active');
+		document.querySelectorAll('.sold__item--' + num + ' .sold__check')[0].classList.add('sold__check--active');
+		document.querySelectorAll('.sold__item--' + num + ' .sold__value')[0].classList.add('sold__value--inactive');
+
+		document.querySelectorAll('.sold__item--' + num)[0].classList.remove('sold__item--active');
+		( num < 5 ) 
+			? document.querySelectorAll('.sold__item--' + (num + 1))[0].classList.add('sold__item--active')
+			: num;
+	};
+
+	var unlockVideos = function() {
+
+		if ( totalSells < 10000 ) {
+			document.querySelectorAll('.sold__item--1')[0].classList.add('sold__item--active');
+		}
+
+		if ( totalSells >= 10000 ) {
+			unlock(1);
+		}
+
+		if ( totalSells >= 25000 ) {
+			unlock(2);
+		}
+
+		if ( totalSells >= 50000 ) {
+			unlock(3);
+		}
+
+		if ( totalSells >= 80000 ) {
+			unlock(4);
+		}
+
+		if ( totalSells >= 120000 ) {
+			unlock(5);
+		}
+
+	};
 
 
 	var init = function() {
 
 		gamesSold();
+		unlockVideos();
 
 	};
 
